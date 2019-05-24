0f039788854b0fcf3b587be193deb46adc0ebc77
Grant Hill <mooman72v2@gmail.com>
2019-05-23 22:03:18 -0700

    Add note about instance URL and don't double register apps.

--- setup/app.go
+++ setup/app.go
@@ -6,6 +6,20 @@ import (
 )
 
 func app(args []string) {
+
+	url, err := common.GetConfig(common.InstanceUrl)
+
+	if err != nil || url == "" {
+		fmt.Printf("Please set your instance URL by running:\n %s setup set config instanceurl https://[your instance url]\n", common.CommandName)
+		return
+	}
+
+	if common.OptionExists(common.ClientId) && common.OptionExists(common.ClientSecret) {
+		fmt.Println("You have already registered an app with", url, ". If this is for a new instance, make a new config file with the -c flag or delete", common.ConfigLocation)
+		fmt.Printf("You probably want to do this next:\n %s setup creds\n", common.CommandName)
+		return
+	}
+
 	resp, err := common.MakePostRequest("/api/v1/apps", map[string]string{
 		"client_name":   "Mastodial",
 		"redirect_uris": "urn:ietf:wg:oauth:2.0:oob",
@@ -31,5 +45,7 @@ func app(args []string) {
 	if err != nil {
 		fmt.Println("Failed to set config. Please try again. Error: ", err.Error())
 	}
+
+	fmt.Println("Registered app!")
 
 }
